c3509bba09868bb3c4fec09ab0e8c92e1a11c6db
oddworldng <el.lobo.4ever@gmail.com>
2012-10-25 23:40:19 +0100

    commit

--- lib/ppt.rb
+++ lib/ppt.rb
@@ -1,12 +1,14 @@
 #!/usr/bin/ruby
 
 class PiedraPapelTijeras
-
+	
 	def humano
 		:piedra
 	end
 
 	def computer
+		lista_resultados = [:piedra, :papel, :tijeras, :piedra, :piedra, :tijeras, :piedra]
+		unico_juego = [:tijeras]
 		:papel
 	end
 
@@ -22,6 +24,48 @@ class PiedraPapelTijeras
 		if tiradas.include?humano
 			return humano
 		end
+	end
+
+	def obtener_maquina
+		if tiradas.include?computer
+			return computer
+		end
+	end
+
+	def jugar 
+		computer
+	end
+
+	def comprobar_todas_tiradas_maquina
+
+		cont_piedra = 0
+		cont_papel = 0
+		cont_tijeras = 0
+
+		while cont_piedra < 1 || cont_papel < 1 || cont_tijeras < 1 do
+			tiradacomputer = tiradas[rand(tiradas.size)]
+			if tiradacomputer == :piedra 
+				cont_piedra += 1
+			elsif tiradacomputer == :papel 
+				cont_papel += 1
+			elsif tiradacomputer == :tijeras
+
+				cont_tijeras += 1
+			end
+
+		end
+
+		return "Se han realizado las tres posibles tiradas"
+	end
+
+	def comprobar_tiradas
+
+		if computer == humano
+			return "Son la misma tirada"
+		else
+			return "No son la misma tirada"
+		end
+
 	end
 
 end	
--- spec/ppt_spec.rb
+++ spec/ppt_spec.rb
@@ -28,21 +28,29 @@ describe PiedraPapelTijeras do
 	end
 
 	it "Se debe invocar al metodo obtener_maquina() para recoger la tirada de la maquina y que esta sea valida\n" do
+		@ppt_obj.obtener_maquina.should == @ppt_obj.computer
 	end
 
-	it "Debe existir una lista de resultados de un juego desde el punto de vista de la maquina\n" do
+	it "Debe existir una lista de resultados de un juego desde el punto de vista de la maquina\n" do # Revisar..
+		#@ppt_obj.computer.lista_resultados.should == [:piedra, :papel, :tijeras, :piedra, :piedra, :tijeras, :piedra]
+		#puts @ppt_obj.lista_resultados
 	end
 
-	it "Debe existir un resultado para un juego, desde el punto de vista de lamaquina\n" do
+	it "Debe existir un resultado para un juego, desde el punto de vista de la maquina\n" do # Revisar..
+		#@ppt_obj.computer.unico_juego.should == [:tijeras]
 	end
 	
-	it "Se debe invocar al metodo jugar() para determinar el ganador de la tirada\n" do
+	it "Se debe invocar al metodo jugar() para determinar el ganador de la tirada\n" do # Revisar..
+		@ppt_obj.jugar.should == @ppt_obj.computer
+		
 	end
 
 	it "Se debe de comprobar que las tiradas de la maquina al ser aleatorias recorren las tres posibilidades\n" do
+		@ppt_obj.comprobar_todas_tiradas_maquina.should == "Se han realizado las tres posibles tiradas"
 	end
 
 	it "Se debe comprobar que las tiradas de la maquina y del humano no son siempre la misma\n" do
+		@ppt_obj.comprobar_tiradas.should == "No son la misma tirada"
 	end 
 
 end
